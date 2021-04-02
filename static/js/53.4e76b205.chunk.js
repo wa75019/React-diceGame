82f0cf7326b08d9bc1db5574e6dcc1a163e2a212
Benoit Laurore <benoit.laurore@bbox.fr>
2021-04-02 15:32:43 +0200

    Updates

---
+++ static/js/53.4e76b205.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpdice=this.webpackJsonpdice||[]).push([[53],{154:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return o}));var i=e(16),c=e(14),o=function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.h)((function(){var t=n.innerWidth,e=n.innerHeight,o=document.elementFromPoint(t/2,e/2);if(o){var r=o.closest("ion-content");r&&new Promise((function(n){return Object(c.c)(r,n)})).then((function(){Object(i.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.4e76b205.chunk.js.map
